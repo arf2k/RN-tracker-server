5ad131cda5653b5f503a3893ccfb86da1a39ab30
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-04-04 16:26:33 -0400

    Create express api and connect mongoose to mongodb set up authroute for post

--- src/index.js
+++ src/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const authRoutes = require("./routes/authRoutes");
 
-const app = express(); 
+const app = express();
+
+app.use(authRoutes);
 
 app.get("/", (req, res) => {
-     res.send("Sup?")
-})
+  res.send("Sup?");
+});
 
 app.listen(3000, () => {
-     console.log("listening to port 3000")
-}) 
+  console.log("listening to port 3000");
+});
+
+const mongoUri =
+  "mongodb+srv://AForeman:bdG8CuTpZB0Xsj5t@cluster0.ixk8f.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("connected to mongo instance");
+});
+mongoose.connection.on("error", (err) => {
+  console.error("Error connecting to mongo", err);
+});
