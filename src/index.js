99b1b2b196e088b39341e73bc94437e3bdeaf4d6
https://learn.co/arf2k <foreman.alexander@gmail.com>
2021-04-04 17:09:56 -0400

    Create User schema and connect to mongo

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
+require("./models/User")
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
 
+app.use(bodyParser.json());
 app.use(authRoutes);
 
 app.get("/", (req, res) => {
+  console.log(req.body);
   res.send("Sup?");
 });
 
